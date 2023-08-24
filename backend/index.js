d5091a0583526546cc43b89c2d101ac77aed08e5
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-08-24 11:06:52 +0100

    updated apis

--- backend/index.js
+++ backend/index.js
@@ -50,27 +50,20 @@ app.post('/api/notes', (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete('/api/notes/:id', (request, response) => {
